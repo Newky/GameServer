168da6881a3e1dc4b3c7534b99eb2e7450f57259
Newky <richdel1991@gmail.com>
2011-09-08 21:10:00 +0000

    Some more unit tests coming but this will do for a minute.

--- test.js
+++ test.js
@@ -1,27 +1,33 @@
-var Game = require("./xo.js").Game;
+var Manager = require("./xo.js").Manager;
 var assert = require("assert");
 
-var instance = new Game();
+var gamesmaster = new Manager();
 
-instance.move(1,1);
-instance.move(0,1);
-instance.move(0,0);
-instance.move(1, 0);
-instance.move(2,2);
+var game_id = gamesmaster.addGame("a", "bc");
+
+assert.ok(game_id == "a9993e364706816aba3e25717850c26c9cd0d89d", "Hash function not working");
+
+/*var instance = new Game();*/
+
+/*instance.move(1,1);*/
+/*instance.move(0,1);*/
+/*instance.move(0,0);*/
+/*instance.move(1, 0);*/
+/*instance.move(2,2);*/
 /*assert.ok(instance.whosTurn() === "Player 1", "Turn Logic Not Working");*/
 
 
 
-assert.ok(instance.over() === "Player 1", "Basic Who Won Doesn't Work");
+/*assert.ok(instance.over() === "Player 1", "Basic Who Won Doesn't Work");*/
 
-var instance2 = new Game( [ [ 0, -1, 0 ], [1, -1, -1] , [1, -1, -1] ], 0 );
+/*var instance2 = new Game( [ [ 0, -1, 0 ], [1, -1, -1] , [1, -1, -1] ], 0 );*/
 
-instance2.move(0, 1);
-assert.ok(instance2.over() === "Player 1", "ReInit Board Doesn't Work");
+/*instance2.move(0, 1);*/
+/*assert.ok(instance2.over() === "Player 1", "ReInit Board Doesn't Work");*/
 
-var instance3 = new Game( [ [0, 0, 0], [1, -1, -1], [-1, -1, -1] ]);
+/*var instance3 = new Game( [ [0, 0, 0], [1, -1, -1], [-1, -1, -1] ]);*/
 
-assert.ok(!instance3.isValidBoard(), "InvalidBoardError doesnt work");
+/*assert.ok(!instance3.isValidBoard(), "InvalidBoardError doesnt work");*/
 
 /*
 Responses:
